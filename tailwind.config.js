b605637bcf3bb9c6fcba11a7e5f72f1c3f9ccea2
Panharoth06 <panharothchheng0903@gmail.com>
2025-05-14 23:37:45 +0700

    update: cpnfig-tailwind-custome-ultility-class

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+// // tailwind.config.js
+// module.exports = {
+//   content: ['./src/**/*.{html,js,jsx,ts,tsx}'], // adjust if needed
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: {
+//           base: '#1d1d1d',
+//           800: '#292929',
+//           700: '#343434',
+//           600: '#3f3f3f',
+//           500: '#4b4b4b',
+//           400: '#565656',
+//           300: '#616161',
+//           200: '#6c6c6c',
+//           150: '#838383',
+//           100: '#999999',
+//           50: '#b0b0b0',
+//         },
+//         secondary: {
+//           base: '#fd8418',
+//           800: '#fd8a24',
+//           700: '#fd902f',
+//           600: '#fd973b',
+//           500: '#fd9d46',
+//           400: '#fda352',
+//           300: '#fea95d',
+//           200: '#feaf69',
+//           150: '#febb80',
+//           100: '#fec18b',
+//           50: '#fecea3',
+//         },
+//       },
+//       fontSize: {
+//         'heading-1': '48px',
+//         'heading-2': '36px',
+//         'heading-3': '30px',
+//         'heading-4': '24px',
+//         'heading-5': '20px',
+//         'heading-6': '16px',
+
+//         'body-large': '18px',
+//         'body': '16px',
+//         'body-small': '14px',
+//         'x-small': '12px',
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
